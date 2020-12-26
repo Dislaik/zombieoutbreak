e0bdb8cfc22e319b239808a6257dad625d8e0c2d
Stex <65769712+stex-work@users.noreply.github.com>
2020-12-26 17:28:49 +0100

    Translation to french of inventory

---
+++ Modules/Core/Inventory/Data/Locales/Fr.lua
@@ -0,0 +1,19 @@
+Translations = {
+    ["No_Player_Nearby"] = "~b~[Info]~s~ Aucun joueur à proximité.",
+    ["Cant_Carry_Item"] = "~y~[Avertissement]~s~La limite d'item est atteinte.",
+    ["Cant_Carry_Weapon"] = "~y~[Avertissement]~sTu disposes déjà de cette arme dans ton inventaire.",
+    ["Encumbered"] = "~y~[Avertissement]~s~ Ton inventaire est plein, tu ne peux plus rien porter.",
+    ["Player_Give_Encumbered"] = "~y~[Avertissement]~s~ Tu as essayé de donner ~y~%s~s~ a ~b~%s~s~, mais il ne peut plus en porter.",
+    ["Target_Give_Encumbered"] = "~y~[Avertissement]~s~ ~b~%s~s~ à essayé de te donner un~y~%s~s~, mais tu ne peux plus en porter.",
+    ["Player_Give_OneItem"] = "~b~[Info]~s~ Tu as donné un ~y~%s~s~ a ~b~%s~s~.",
+    ["Target_Give_OneItem"] = "~b~[Info]~s~ Tu as reçu un ~y~%s~s~ de ~b~%s~s~.",
+    ["Player_Give_Item"] = "~b~[Info]~s~ Tu as donné ~y~%s~s~ x%s à ~b~%s~s~.",
+    ["Target_Give_Item"] = "~b~[Info]~s~ Tu as reçu un ~y~%s~s~ x%s de ~b~%s~s~.",
+    ["Player_Give_CantTakeMore"] = "~y~[Avertissement]~s~ Tu as essayé de donner ~y~%s~s~ à ~b~%s~s~, mais il ne peut plus en porter.",
+    ["Target_Give_CantTakeMore"] = "~y~[Avertissement]~s~ ~b~%s~s~  a essayé de te donner ~y~%s~s~, mais tu ne peux plus en porter.",
+    ["Player_Give_Weapon"] = "~b~[Info]~s~ Tu as donné ~y~%s~s~ à ~b~%s~s~.",
+    ["Target_Give_Weapon"] = "~b~[Info]~s~ Tu as reçu ~y~%s~s~ de ~b~%s~s~.",
+    ["Player_Give_HasWeapon"] = "~y~[Info]~s~ Tu as essayé de donner un ~y~%s~s~ à ~b~%s~s~,mais il dispose déjà.",
+    ["Target_Give_HasWeapon"] = "~y~[Avertissement]~s~ ~b~%s~s~ a essayé de te donner un ~y~%s~s~, mais tu en disposes déjà d'un.",
+    ["Clothes_Not_Available"] = "~y~[Avertissement]~s~ ~y~%s~s~ cela n'est aps disponible pour ton personnage."
+}
