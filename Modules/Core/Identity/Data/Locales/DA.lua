b27d7871bce6b51ab13d7cd7f99c7cce8b33c1ad
GolfDev-hub <76699473+GolfDev-hub@users.noreply.github.com>
2021-01-02 16:35:35 +0100

    DA & EN Translations

---
+++ Modules/Core/Identity/Data/Locales/DA.lua
@@ -0,0 +1,8 @@
+Translations = {
+    ["Unemployed"] = "Arbejdsløs",
+    ["Unemployed_Description"] = "Beskrivelse af en arbejdsløs",
+    ["Police"] = "Politi",
+    ["Police_Description"] = "Beskrivelse af en politibetjent",
+    ["Paramedic"] = "Læge",
+    ["Paramedic_Description"] = "Beskrivelse af en læge.",
+}
